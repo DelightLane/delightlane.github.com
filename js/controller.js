c6b7caa0a3fbe05596570fff4b127d29b90dd3fe
DelightLane <hano881112@hanmir.com>
2017-07-18 16:02:31 +0900

    마우스 및 터치로도 이동시킬 수 있도록 코드 적용

--- js/controller.js
+++ js/controller.js
@@ -11,6 +11,52 @@ document.body.onkeydown = function( e ) {
     }
 };
 
+
+function getMousePos(canvasDom, mouseEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: mouseEvent.clientX - rect.left,
+    y: mouseEvent.clientY - rect.top
+  };
+}
+
+canvas.addEventListener("mouseup", function (e) {
+    var pos = getMousePos(canvas, e);
+    pos.x = (canvas.width / 2) - pos.x;
+    pos.y = (canvas.height / 2) - pos.y;
+
+    var directTarget = Math.abs(pos.x) > Math.abs(pos.y) ? pos.x : pos.y;
+
+    if(pos.x == directTarget)
+    {
+        if(directTarget > 0)
+        {
+            onKeyDown('left');
+        }
+        else
+        {
+            onKeyDown('right');
+        }
+    }
+    else
+    {
+        if(directTarget > 0)
+        {
+            onKeyDown('up');
+        }
+        else
+        {
+            onKeyDown('down');
+        }
+    }
+    render();
+}, false);
+
+canvas.addEventListener("touchend", function (e) {
+    var mouseEvent = new MouseEvent("mouseup", {});
+    canvas.dispatchEvent(mouseEvent);
+}, false);
+
 function onKeyDown( key ){
     onKeyDownMap(key);
 }
